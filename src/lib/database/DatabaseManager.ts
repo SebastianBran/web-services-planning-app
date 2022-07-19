78ed7a1dc08e703d349ea310be65d08ef6549b80
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-07-18 22:27:41 -0500

    active ssl in databaseManager

--- src/lib/database/DatabaseManager.ts
+++ src/lib/database/DatabaseManager.ts
@@ -18,6 +18,7 @@ export default class DatabaseManager {
           database: process.env.PG_DATABASE,
           entities: [__dirname + "/../**/*.entity.{js,ts}"],
           synchronize: true,
+          ssl: true,
         });
       } else {
         console.log("NODE_ENV is test or development");
